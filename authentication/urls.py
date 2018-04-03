ad677df23f761da203b33a5a4a78b58f4539814d
joshpaulchan <joshpaulchan@gmail.com>
2018-04-03 00:10:34 -0400

    Implement login view and swap urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,14 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import (
-    UserCreateAPIView,
-    UserLoginAPIView
-    )
+from . import views
 
 urlpatterns = [
-    url(r'^login/$', UserLoginAPIView.as_view(), name='login'),
-    url(r'^register/$', UserCreateAPIView.as_view(), name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^register/$', views.UserCreateAPIView.as_view(), name='register'),
 ]
 # from django.conf.urls import include, url
 # from .views import AuthLogin, AuthRegister
--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,22 @@
 from django.db.models import Q
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+User = get_user_model()
+
+from rest_framework import decorators
 
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
+from rest_framework_jwt.settings import api_settings
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 from rest_framework.filters import (
-        SearchFilter,
-        OrderingFilter,
-    )
+    SearchFilter,
+    OrderingFilter,
+)
 
 from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin
 from rest_framework.generics import (
@@ -19,14 +26,20 @@ from rest_framework.generics import (
     UpdateAPIView,
     RetrieveAPIView,
     RetrieveUpdateAPIView
-    )
+)
 from rest_framework.permissions import (
     AllowAny,
     IsAuthenticated,
     IsAdminUser,
     IsAuthenticatedOrReadOnly,
+)
 
-    )
+from rest_framework.pagination import (
+    LimitOffsetPagination,
+    PageNumberPagination,
+)
+
+from . import serializers
 
 # from posts.api.permissions import IsOwnerOrReadOnly
 from rest_framework.permissions import BasePermission, SAFE_METHODS
@@ -49,45 +62,89 @@ class IsOwnerOrReadOnly(BasePermission):
 
 # from posts.api.pagination import PostLimitOffsetPagination, PostPageNumberPagination
 
-from rest_framework.pagination import (
-    LimitOffsetPagination,
-    PageNumberPagination,
-    )
-
-
-
 class PostLimitOffsetPagination(LimitOffsetPagination):
     default_limit = 10
     max_limit = 10
 
-
 class PostPageNumberPagination(PageNumberPagination):
     page_size = 20
 
-
-    
-User = get_user_model()
-
-
-from .serializers import (
-    UserCreateSerializer,
-    UserLoginSerializer,
-    )
-
-
 class UserCreateAPIView(CreateAPIView):
-    serializer_class = UserCreateSerializer
+    serializer_class = serializers.UserCreateSerializer
     queryset = User.objects.all()
     permission_classes = [AllowAny]
 
-class UserLoginAPIView(APIView):
-    permission_classes = [AllowAny]
-    serializer_class = UserLoginSerializer
+# class UserLogin(APIView):
+#     """
+#     View to log users into the API
+#     """
+#     serializer_class = serializers.UserLoginSerializer
+#     # permission_classes = [AllowAny]
     
-    def post(self, request, *args, **kwargs):
-        data = request.data
-        serializer = UserLoginSerializer(data=data)
-        if serializer.is_valid(raise_exception=True):
-            new_data = serializer.data
-            return Response(new_data, status=HTTP_200_OK)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+#     # Logs a user in
+#     def post(self, request):
+#         """
+#         Logs a user in to the server
+#         """
+#         data = request.data
+#         serializer = serializers.UserLoginSerializer(data=data)
+#         if serializer.is_valid(raise_exception=True):
+#             print(serializer.data)
+#             new_data = serializer.data
+#             return Response(new_data, status=HTTP_200_OK)
+#         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+@decorators.api_view(['POST'])
+@decorators.permission_classes([AllowAny])
+def register(req):
+    """
+    Registers a user with the application.
+
+    @param      Request     req             RF's Request object
+
+    @post       str         req.email       email of user logging in
+    @post       str         req.password    password of user logging in
+    @returns    Response
+    """
+    raise NotImplementedError
+
+@decorators.api_view(['POST'])
+@decorators.permission_classes([AllowAny])
+def login(req):
+    """
+    Logs a user in to the server.
+
+    @param      Request     req             RF's Request object
+
+    @post       str         req.email       email of user logging in
+    @post       str         req.password    password of user logging in
+    @returns    Response
+    """
+    # TODO: validate for req.data.email
+    email = req.data.get("email")
+    password = req.data.get("password")
+
+    # fetch user if user w email existts and verify pw
+    user = authenticate(username=email, password=password)
+    
+    if user is not None:
+        resp = {}
+
+        # serialize user object
+        serializer = serializers.UserLoginSerializer(user)
+        resp["user"] = dict.copy(serializer.data)
+
+        # generato token
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        resp["token"] = token
+        return Response(resp)
+
+    return Response({
+        "error": {
+            "code": "AUTH/INCORRECT_EMAIL_PASSWORD_COMBINATION",
+            "message": "That email and password combination was incorrect."
+        }
+    }, status=HTTP_400_BAD_REQUEST)
