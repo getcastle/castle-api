f6f709463de886487b38192a50099cc643e1a2c1
joshpaulchan <joshpaulchan@gmail.com>
2018-04-03 00:08:39 -0400

    Add some comments to models

--- authentication/models.py
+++ authentication/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
-
 class Profile(models.Model):
-    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
     firstName= models.CharField(max_length=200)
     lastName= models.CharField(max_length=200)
     phoneNumber= models.CharField(max_length=10)
@@ -17,14 +16,18 @@ class Profile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Creates and links additional Profile object on User object creation.
+    """
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+    """
+    Updates Profile object after User object is saved"
+    """
     instance.profile.save()
-
-
 
 # class AccountManager(BaseUserManager):
 #     def create_user(self, email, password=None, **kwargs):
