6255a0b5a507734380ed68843480800dc4fbeb10
joshpaulchan <joshpaulchan@gmail.com>
2018-04-06 15:08:20 -0400

    Add token verification and refreshing routes

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,28 +2,12 @@ from django.urls import path
 from django.conf.urls import url
 from django.contrib import admin
 
+from rest_framework_jwt import views as jwt_views
 from . import views
 
 urlpatterns = [
     path('login', views.login, name='auth.login'),
     path('register', views.UserRegistrar.as_view(), name='auth.register'),
+    path('token/verify', jwt_views.verify_jwt_token, name='auth.token.verify'),
+    path('token/refresh', jwt_views.refresh_jwt_token, name='auth.token.refresh'),
 ]
-# from django.conf.urls import include, url
-# from .views import AuthLogin, AuthRegister
-# from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
-
-# urlpatterns = [
-#     url(r'^login/', obtain_jwt_token),
-#     url(r'^token-refresh/', refresh_jwt_token),
-#     url(r'^token-verify/', verify_jwt_token),
-#     url(r'^register/$', AuthRegister.as_view()),
-
-#     # URL Created for manual login logic
-#     # url(r'^login/$', AuthLogin.as_view()),
-# ]
-
-
-
-
-
-
