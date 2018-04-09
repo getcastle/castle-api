a4ba98209d42f6cc6b5e1919643cde114560bb8e
joshpaulchan <joshpaulchan@gmail.com>
2018-04-09 14:47:01 -0400

    Clean up household serializers

--- households/serializers.py
+++ households/serializers.py
@@ -1,20 +1,9 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-User = get_user_model()
-from .models import Household, HouseholdMember
+from .models import Household
 
-from authentication.serializers import UserSerializer
-
-class HouseholdListSerializer(serializers.ModelSerializer):
+class HouseholdSerializer(serializers.ModelSerializer):
     
     class Meta:
       model = Household
       exclude = ('members',)
-  
-class HouseholdDetailSerializer(serializers.ModelSerializer):
-  members = UserSerializer(many=True)
-
-  class Meta:
-    model = Household
-    fields = '__all__'
