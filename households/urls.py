313e902fa147aea020a50eae6cb2a2a367ffa64f
joshpaulchan <joshpaulchan@gmail.com>
2018-04-09 14:48:19 -0400

    Fix household urls

--- households/urls.py
+++ households/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
   path('households', views.HouseholdList.as_view(), name='households.list'),
   path('household/<int:pk>', views.HouseholdDetail.as_view(), name='household.detail'),
-  path('household/<int:pk>/devices', views.HouseholdDetail.as_view(), name='household.devices'),
 ]
